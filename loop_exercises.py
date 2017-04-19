68d46e3441e206c1383641e2b60fdfdc326da37f
Aspen Hollyer <aspen.dollins@gmail.com>
2017-04-19 08:04:17 -0500

    Create loop_exercises.pyk

--- loop_exercises.py
+++ loop_exercises.py
@@ -36,3 +36,28 @@ for i in range(0, side):
 
 # 6. Print a Box
 print("\n6. Print a Box:")
+height = int(input("How tall is the box? Enter an integer: "))
+width = int(input("How wide is the box? Enter an integer: "))
+def print_box(w, h):
+    print("*" * w)
+    for i in range(0, (h-2)):
+        print("*" + (" " * (w-2)) + "*")
+    print("*" * w)
+print_box(width, height)
+
+# 7. Print a Triangle
+(print("\n7. Print a Triangle:"))
+print(" " * 3 + "*" * 1 + " " * 3)
+print(" " * 2 + "*" * 3 + " " * 2)
+print(" " * 1 + "*" * 5 + " " * 1)
+print(" " * 0 + "*" * 7 + " " * 0)
+
+# 8. Print a Triangle II
+print("\n8. Print a Triangle II:")
+height = int(input("What is the triangle's height? Enter an integer: "))
+def print_triangle(h):
+    num_stars = 1
+    for i in range(0, h):
+        print(" " * (h-i) + "*" * num_stars + " " * (h-i))
+        num_stars += 2
+print_triangle(height)
