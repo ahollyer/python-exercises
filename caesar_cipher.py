f31ae7efd582a088a158b540f610745b9b27bc6b
Aspen Hollyer <aspen.dollins@gmail.com>
2017-04-19 09:34:22 -0500

    Create caesar_cipher.py

---
+++ caesar_cipher.py
@@ -0,0 +1,53 @@
+# caesar_cipher.py
+# By Aspen
+# Accepts a string and amount to shift, returns shifted string
+
+# 6. Caesar Cipher
+
+# Choose a string to translate
+str = "hello hi wxyz"
+# str = input("Enter a string to shift: ").lower()
+
+
+# Determine the amount to shift
+shift_key = 3
+# shift_key = int(input("How much do you want to shift? Enter an integer. Positive numbers encrypt, while negative numbers decrypt: "))
+
+
+
+# Create a function that accepts the string & shift key
+def c_shift(str, shift_key):
+
+    # Create a list of characters to use indices for shifting
+    chars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+    # Create an empty string to add shifted chars
+    shifted_str = ""
+
+    # Iterate over each character in the input string
+    for char in str:
+
+        # Don't shift spaces; just add to shifted_str
+        if char == " ":
+            shifted_str += char
+            continue
+        else:
+            # Look up the character in chars
+            i = chars.index(char)
+
+        # Apply the shift key
+        try:
+            c = chars[i + shift_key]
+        except IndexError:
+            if i + shift_key < 0:
+                c = chars[26 + (i + shift_key)]
+            elif i + shift_key > 25:
+                c = chars[i + shift_key - 26]
+
+        # Append the shifted character to shifted_str
+        shifted_str += c
+    return shifted_str
+
+print(str)
+print("Shifted:", c_shift(str, shift_key))
+print("Unshifted:", c_shift(c_shift(str, shift_key), -shift_key))
