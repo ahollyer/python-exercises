b61a5c85ed79423ef3149eae6c718301e666afe7
Aspen Hollyer <aspen.dollins@gmail.com>
2017-04-20 11:21:56 -0500

    Fix main()

--- degree_conversion.py
+++ degree_conversion.py
@@ -4,20 +4,17 @@
 
 print("***\nINSTRUCTIONS: This program converts between Celsius and Fahrenheit. First, enter your units, then enter the temperature.\n***\n")
 
-def main():
+def convert():
+    input_units = input("Starting units? Please type C or F, then press Enter: ").upper()
+    input_temp = float(input("Please type the temperature (ex. 32), then press Enter: "))
 
-    def convert():
-        input_units = input("Starting units? Please type C or F, then press Enter: ").upper()
-        input_temp = float(input("Please type the temperature (ex. 32), then press Enter: "))
-
-        if input_units == "C":
-            converted_units = "F"
-            converted_temp = input_temp * (9/5) + 32
-        elif input_units == "F":
-            converted_units = "C"
-            converted_temp = (input_temp - 32) * (5/9)
+    if input_units == "C":
+        converted_units = "F"
+        converted_temp = input_temp * (9/5) + 32
+    elif input_units == "F":
+        converted_units = "C"
+        converted_temp = (input_temp - 32) * (5/9)
         print("Your converted temperature is:", converted_temp, converted_units)
 
-    convert()
-
-main()
+if __name__ == "__main__":
+  convert()
