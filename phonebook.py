f53ca702104163b1dd6cefdabf9321bdf8535b9b
Aspen Hollyer <aspen.dollins@gmail.com>
2017-04-25 09:42:53 -0500

    Set up phonebook.py structure

--- phonebook.py
+++ phonebook.py
@@ -25,31 +25,34 @@ def quit():
     in_use = False
 
 def use_phonebook():
-    functions = {
-        '1': lookup_entry(),
-        '2': set_entry(),
-        '3': delete_entry(),
-        '4': list_entries(),
-        '5': quit,
-    }
     in_use = True
 
+    functions = {
+        '1': lookup_entry,
+        '2': set_entry,
+        '3': delete_entry,
+        '4': list_entries,
+        '5': quit
+    }
+
+    instructions = 'Electronic Phone Book\n\
+    =====================\n\
+    1. Look up an entry\n\
+    2. Set an entry\n\
+    3. Delete an entry\n\
+    4. List all entries\n\
+    5. Quit\n'
+
+
     while in_use:
-        print('Electronic Phone Book\n\
-        =====================\n\
-        1. Look up an entry\n\
-        2. Set an entry\n\
-        3. Delete an entry\n\
-        4. List all entries\n\
-        5. Quit\n')
+        print(instructions)
 
-        try:
-            ans = int(input("What do you want to do (1-5)? "))
-        except ValueError:
-            print("\nYour input must be a number.")
-
-        functions.get(ans)
-
+        ans = str(input("What do you want to do (1-5)? "))
+        
+        if ans in functions:
+            functions[ans]()
+        else:
+            print("That is not a valid input. Try again.")
 
 if __name__ == '__main__':
     use_phonebook()
